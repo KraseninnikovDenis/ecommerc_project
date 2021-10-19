c29fb2b16e2eeff0f874197a86362bfaedb98b21
Денис <Krasheninnikov.0792@gmail.com>
2021-10-19 21:46:26 +0300

    2021-10-19_21-46

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'shop.apps.ShopConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'commerce',
+        'USER': 'postgres',
+        'PASSWORD':'11235813Krosh11235813',
+        'HOST':'localhost',
+        'PORT' : '5432',
     }
 }
 
