105279ff0c81b35511b0e0fcd8720ab850bb3cbe
Денис <Krasheninnikov.0792@gmail.com>
2021-10-26 21:28:38 +0300

    2021-10-26_21-28

--- ecommerce/shop/models.py
+++ ecommerce/shop/models.py
@@ -1,25 +1,36 @@
 from django.db import models
 
 class Category (models.Model):
-    name = models.CharField(max_length=250, unique=True)
-    slug = models.SlugField(max_length=250, unique=True)
-    discription= models.TextField(blank=True)
-    image= models.ImageField(upload_to='category', blank=True)
+    name = models.CharField(max_length=250, unique=True, verbose_name='название категории')
+    slug = models.SlugField(max_length=250, unique=True, verbose_name='слаг')
+    discription= models.TextField(blank=True, verbose_name='описание категории')
+    image= models.ImageField(upload_to='category', blank=True, verbose_name='картинка')
+
+    class Meta:
+        #ordering=('name')
+        verbose_name='категория'
+        verbose_name_plural='категории'
+
 
     def __str__(self):
         return self.name
 
 class Product (models.Model):
-    name = models.CharField(max_length=250, unique=True)
-    slug = models.SlugField(max_length=250, unique=True)
-    discription= models.TextField(blank=True)
-    image= models.ImageField(upload_to='product', blank=True)
-    category=models.ForeignKey(Category,on_delete=models.CASCADE)
-    price=models.DecimalField(max_digits=10,decimal_places=2)
-    stok=models.IntegerField()
-    available=models.BooleanField(default=True)
+    name = models.CharField(max_length=250, unique=True, verbose_name='название продукта')
+    slug = models.SlugField(max_length=250, unique=True, verbose_name='слаг')
+    discription= models.TextField(blank=True, verbose_name='описание продукта')
+    image= models.ImageField(upload_to='product', blank=True, verbose_name='картинка продукта')
+    category=models.ForeignKey(Category,on_delete=models.CASCADE, verbose_name='категория')
+    price=models.DecimalField(max_digits=10,decimal_places=2, verbose_name='цена продукта')
+    stok=models.IntegerField(verbose_name='количество продукта')
+    available=models.BooleanField(default=True, verbose_name='наличие продукта продукта')
     created=models.DateTimeField(auto_now_add=True)
     updated=models.DateTimeField(auto_now=True)
 
+    class Meta:
+        #ordering=('name')
+        verbose_name='продукт'
+        verbose_name_plural='продукты'
+   
     def __str__(self):
         return self.name
--- ecommerce/shop/views.py
+++ ecommerce/shop/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from .models import Category, Product
+from django.shortcuts import get_object_or_404
 
 
-def home (request):
-    return render(request, 'shop/home.html')
+def home (request, category_slug=None):
+    category_page=None
+    products=None
+    if category_slug !=None:
+        category_page=get_object_or_404(Category,slug=category_slug)
+        products=Product.category.filter(category=category_page, available=True)
+    else:
+        products=Product.objects.all().filter(available=True)
+    return render(request, 'shop/home.html', {'products':products,'category':category_page})
 
 def product (request):
     return render(request, 'shop/product.html')
